8a7adc6fa15bb13a608be110a3b51161bcb299ea
mahdy zahran <ali-mahdy@windowslive.com>
2025-03-14 23:13:11 +0800

    including documentation

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    transform: true,
-  }));
-  await app.listen(process.env.PORT ?? 3000);
+  // Enable CORS
+  app.enableCors({
+    origin: 'http://localhost:5173', // Allow only your frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true, // If you're using cookies or Authorization headers
+  });
+
+  await app.listen(3000);
 }
 bootstrap();
